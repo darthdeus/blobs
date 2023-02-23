559ad534ffbda06c7664ac5ba14da131df18fff1
Jakub Arnold <darthdeus@gmail.com>
2023-02-24 00:08:33 +0100

    WIP blob physics

--- blobs/src/lib.rs
+++ blobs/src/lib.rs
@@ -15,6 +15,22 @@ pub struct RigidBody {
     pub position: Vec2,
     pub rotation: f32,
     pub scale: Vec2,
+
+    pub velocity: Vec2,
+    pub angular_velocity: f32,
+
+    pub colilders: Vec<Collider>,
 }
 
-pub struct BlobPhysics {}
+pub struct BlobPhysics {
+    bodies: Vec<RigidBody>,
+}
+
+impl BlobPhysics {
+    pub fn step(&mut self, delta: f32) {
+        for body in self.bodies.iter_mut() {
+            body.position += body.velocity * delta;
+            body.rotation += body.angular_velocity * delta;
+        }
+    }
+}
