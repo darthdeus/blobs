2c50db17df5d2e944582e59ed034b286d9287453
Jakub Arnold <darthdeus@gmail.com>
2023-03-22 02:08:28 +0100

    Tweaks

--- blobs/src/spatial.rs
+++ blobs/src/spatial.rs
@@ -1,18 +1,17 @@
 use glam::Vec2;
-use std::collections::hash_map::Entry;
 use std::collections::{HashMap, HashSet};
 
 pub type CellIndex = (i32, i32);
 pub type Id = u64;
 
 #[derive(Copy, Clone, Debug)]
-pub struct CellPoint {
-    pub id: Id,
-    pub radius: f32,
+pub struct Point {
+    pub id: u32,
     pub position: Vec2,
+    pub radius: f32,
 }
 
-impl PartialEq for CellPoint {
+impl PartialEq for Point {
     fn eq(&self, other: &Self) -> bool {
         self.id == other.id
     }
@@ -20,75 +19,119 @@ impl PartialEq for CellPoint {
 
 pub struct SpatialHash {
     pub cell_size: f32,
-    pub next_id: u64,
-    pub hash_map: HashMap<CellIndex, Vec<CellPoint>>,
+    pub next_id: u32,
+    pub points: HashMap<u32, Point>,
+    pub grid: HashMap<(i32, i32), HashSet<u32>>,
 }
 
 impl SpatialHash {
     pub fn new(cell_size: f32) -> Self {
-        Self { cell_size, next_id: 0, hash_map: HashMap::new() }
+        Self {
+            cell_size,
+            next_id: 0,
+            points: HashMap::new(),
+            grid: HashMap::new(),
+        }
     }
 
-    pub fn hash_point(&self, point: Vec2) -> (i32, i32) {
+    fn get_cell_coords(&self, position: Vec2) -> (i32, i32) {
         (
-            (point.x / self.cell_size).floor() as i32,
-            (point.y / self.cell_size).floor() as i32,
+            (position.x / self.cell_size).floor() as i32,
+            (position.y / self.cell_size).floor() as i32,
         )
     }
 
-    pub fn insert(&mut self, point: Vec2, radius: f32) -> CellPoint {
+    pub fn insert(&mut self, position: Vec2, radius: f32) -> u32 {
         let id = self.next_id;
         self.next_id += 1;
 
-        let point = CellPoint { id, position: point, radius };
+        let point = Point { id, position, radius };
+        let cell_coords = self.get_cell_coords(point.position);
 
-        self.insert_with_id(point);
-        point
+        self.grid
+            .entry(cell_coords)
+            .or_insert_with(HashSet::new)
+            .insert(point.id);
+        self.points.insert(point.id, point);
+
+        id
     }
 
-    fn insert_with_id(&mut self, point: CellPoint) {
-        let hash = self.hash_point(point.position);
-
-        match self.hash_map.entry(hash) {
-            Entry::Occupied(mut entry) => entry.get_mut().push(point),
-            Entry::Vacant(entry) => {
-                entry.insert(vec![point]);
+    pub fn remove(&mut self, id: u32) -> Option<Point> {
+        if let Some(point) = self.points.remove(&id) {
+            let cell_coords = self.get_cell_coords(point.position);
+            if let Some(cell) = self.grid.get_mut(&cell_coords) {
+                cell.remove(&id);
             }
+            Some(point)
+        } else {
+            None
         }
     }
 
-    pub fn remove(&mut self, point: &CellPoint) -> bool {
-        let hash = self.hash_point(point.position);
+    pub fn move_point(&mut self, id: u32, offset: Vec2) -> Option<()> {
+        if let Some(point) = self.points.get(&id) {
+            let old_position = point.position;
+            let new_position = old_position + offset;
 
-        if let Some(points) = self.hash_map.get_mut(&hash) {
-            if let Some(index) = points.iter().position(|&p| p.id == point.id) {
-                points.swap_remove(index);
-                return true;
+            let old_cell_coords = self.get_cell_coords(old_position);
+            let new_cell_coords = self.get_cell_coords(new_position);
+
+            if old_cell_coords != new_cell_coords {
+                if let Some(cell) = self.grid.get_mut(&old_cell_coords) {
+                    cell.remove(&id);
+                }
+                self.grid
+                    .entry(new_cell_coords)
+                    .or_insert_with(HashSet::new)
+                    .insert(id);
             }
-        }
 
-        false
+            if let Some(point) = self.points.get_mut(&id) {
+                point.position = new_position;
+            }
+            Some(())
+        } else {
+            None
+        }
     }
 
-    pub fn query(&self, point: Vec2, radius: f32) -> Vec<CellPoint> {
-        let mut results = Vec::new();
-        let min_hash = self.hash_point(point - Vec2::splat(radius));
-        let max_hash = self.hash_point(point + Vec2::splat(radius));
+    fn get_neighbor_cells(&self, cell_coords: (i32, i32)) -> Vec<(i32, i32)> {
+        let (x, y) = cell_coords;
+        vec![
+            (x - 1, y - 1),
+            (x - 1, y),
+            (x - 1, y + 1),
+            (x, y - 1),
+            (x, y),
+            (x, y + 1),
+            (x + 1, y - 1),
+            (x + 1, y),
+            (x + 1, y + 1),
+        ]
+    }
 
-        for x in min_hash.0..=max_hash.0 {
-            for y in min_hash.1..=max_hash.1 {
-                if let Some(points) = self.hash_map.get(&(x, y)) {
-                    for &p in points {
-                        if (p.position - point).length() <= (radius + p.radius)
-                        {
-                            results.push(p);
-                        }
+    pub fn query_with_cells(
+        &self,
+        position: Vec2,
+        query_radius: f32,
+    ) -> (Vec<Point>, Vec<(i32, i32)>) {
+        let cell_coords = self.get_cell_coords(position);
+        let neighbor_cells = self.get_neighbor_cells(cell_coords);
+
+        let mut points_within_radius = Vec::new();
+        for cell_coords in &neighbor_cells {
+            if let Some(cell) = self.grid.get(cell_coords) {
+                for point_id in cell {
+                    let point = self.points.get(point_id).unwrap();
+                    if (point.position - position).length() - point.radius <=
+                        query_radius
+                    {
+                        points_within_radius.push(point.clone());
                     }
                 }
             }
         }
-
-        results
+        (points_within_radius, neighbor_cells)
     }
-
 }
